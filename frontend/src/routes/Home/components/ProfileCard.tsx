2bbe1ae643745f5bee36fb621f193605044a9dcd
Pogrammerr <84626499+Pogrammerr@users.noreply.github.com>
2022-12-15 12:37:33 +0300

    -

--- frontend/src/routes/Home/components/ProfileCard.tsx
+++ frontend/src/routes/Home/components/ProfileCard.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Flex, Card, Text } from 'components'
-import UserImg from 'assets/user.png'
-import { FaGraduationCap } from 'react-icons/fa'
+import React from "react";
+import { Flex, Card, Text } from "components";
+import UserImg from "assets/user.png";
+import { FaGraduationCap } from "react-icons/fa";
+import { useUser } from "state/hooks";
 
 const ProfileCard = () => {
+  const user = useUser();
   return (
-    <Card size='sm' flexDirection='row'>
+    <Card size="sm" flexDirection="row">
       <img src={UserImg} alt="User Picture" width={64} />
-      <Flex flexDirection='column'>
-        <Text bold>Can Özfuttu <FaGraduationCap /> </Text>
+      <Flex flexDirection="column">
+        <Text bold>
+          {user.firstName} {user.lastName} <FaGraduationCap />{" "}
+        </Text>
         <Text>Bilgisayar Mühendisliği</Text>
       </Flex>
     </Card>
-  )
-}
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
